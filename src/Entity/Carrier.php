5d6481edf0211ff63d8095c9ec6d581f884e61d8
Ignas <statkus.online@gmail.com>
2024-02-18 21:05:16 +0200

    created 'Carrier' class

---
+++ src/Entity/Carrier.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Invertus\Academy\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Invertus\Academy\Repository\CarrierRepository;
+
+#[ORM\Entity(repositoryClass: CarrierRepository::class)]
+class Carrier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
