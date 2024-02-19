32fbc1e49475f86c2e89c736411423f817752345
MarijusCoding <marijusdilys9@gmail.com>
2024-02-19 10:52:08 +0200

    Fixed make commands by changing namespaces from Invertus/Academy to App/ 	modified:   bin/console 	modified:   composer.lock 	modified:   config/services.yaml 	modified:   public/index.php 	modified:   src/Kernel.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Invertus\Academy\Kernel;
+use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Invertus\Academy;
+namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
