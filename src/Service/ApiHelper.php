20b1787f9837047b0b4f5eb017e48d105986de66
Steponas <steponaskam@gmail.com>
2024-03-11 23:17:08 +0200

    added getData() fnc to ApiHelper service

--- src/Service/ApiHelper.php
+++ src/Service/ApiHelper.php
@@ -2,7 +2,13 @@
 
 namespace Invertus\Academy\ApiHelper;
 
+
+use Symfony\Component\HttpFoundation\Request;
+
 class ApiHelper
 {
-
+    public function getData(Request $request): array
+    {
+        return json_decode($request->getContent(), true);
+    }
 }
