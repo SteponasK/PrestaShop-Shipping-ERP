cb55d7275bf5ea611ce51863fca1b52a81ac1996
Steponas <steponaskam@gmail.com>
2024-03-11 23:21:06 +0200

    added isApiKeyValid() fnc to ApiHelper service

--- src/Service/ApiHelper.php
+++ src/Service/ApiHelper.php
@@ -11,4 +11,15 @@ class ApiHelper
     {
         return json_decode($request->getContent(), true);
     }
+
+    public function isApiKeyValid(Request $request): bool
+    {
+        $authorizationHeader = $request->headers->get('Authorization');
+        $token = str_replace('Bearer ', '', $authorizationHeader);
+        if ($_ENV['API_KEY'] !== $token)
+        {
+            return false;
+        }
+        return true;
+    }
 }
