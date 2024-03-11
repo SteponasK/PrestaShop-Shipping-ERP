15e79f7220d521ab24e1bcf7e49ab58ce821c4dc
Steponas <steponaskam@gmail.com>
2024-03-11 21:14:23 +0200

    Moved createShipment function to a service

--- src/Controller/ShipmentController.php
+++ src/Controller/ShipmentController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Invertus\Academy\Controller;
-
 use Doctrine\ORM\EntityManagerInterface;
 use Invertus\Academy\Entity\Product;
 use Invertus\Academy\Entity\Shipment;
@@ -11,13 +10,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Dompdf\Dompdf;
+use Invertus\Academy\ShipmentCreateService\ShipmentCreateService;
 use Picqer\Barcode\BarcodeGeneratorHTML;
 use Picqer\Barcode\BarcodeGeneratorPNG;
 
 class ShipmentController extends AbstractController
 { 
     #[Route('/api/shipment/save/', name: 'app_save_shipment', methods: ['POST'])]
-    public function save(Request $request, EntityManagerInterface $entityManager): Response
+    public function save(Request $request, EntityManagerInterface $entityManager, ShipmentCreateService $service): Response
     {
         $authorizationHeader = $request->headers->get('Authorization');
         $token = str_replace('Bearer ', '', $authorizationHeader);
@@ -26,7 +26,7 @@ class ShipmentController extends AbstractController
         }
  
         $data = json_decode($request->getContent(), true);
-        $this->createShipment($data, $entityManager);
+        $service->createShipment($data, $entityManager);
         
         return new Response('', Response::HTTP_CREATED);
     }
@@ -113,24 +113,6 @@ class ShipmentController extends AbstractController
             'phoneMobile' => $shipment->getPhoneMobile(),
             'barcode' => $shipment->getBarcode()
         ];
-    }
-    private function createShipment(array $data, EntityManagerInterface $entityManager): void
-    {
-        $shipment = new Shipment();
-        $shipment->setCountry($data['country']);
-        $shipment->setCompany($data['company']);
-        $shipment->setFirstName($data['firstName']);
-        $shipment->setLastName($data['lastName']);
-        $shipment->setAddress1($data['address1']);
-        $shipment->setAddress2($data['address2']);
-        $shipment->setPostcode($data['postcode']);
-        $shipment->setCity($data['city']);
-        $shipment->setPhone($data['phone']);
-        $shipment->setPhoneMobile($data['phoneMobile']);
-        $shipment->setBarcode(decbin(time()));
-
-        $entityManager->persist($shipment);
-        $entityManager->flush();
     }
     
 }
--- src/Service/ShipmentCreateService.php
+++ src/Service/ShipmentCreateService.php
@@ -2,7 +2,26 @@
 
 namespace Invertus\Academy\ShipmentCreateService;
 
+use Invertus\Academy\Entity\Shipment;
+use Doctrine\ORM\EntityManagerInterface;
+use Invertus\Academy\Controller;
 class ShipmentCreateService
 {
-    
+    public function createShipment(array $data, EntityManagerInterface $entityManager) : void {
+        $shipment = new Shipment();
+        $shipment->setCountry($data['country']);
+        $shipment->setCompany($data['company']);
+        $shipment->setFirstName($data['firstName']);
+        $shipment->setLastName($data['lastName']);
+        $shipment->setAddress1($data['address1']);
+        $shipment->setAddress2($data['address2']);
+        $shipment->setPostcode($data['postcode']);
+        $shipment->setCity($data['city']);
+        $shipment->setPhone($data['phone']);
+        $shipment->setPhoneMobile($data['phoneMobile']);
+        $shipment->setBarcode(decbin(time()));
+
+        $entityManager->persist($shipment);
+        $entityManager->flush();
+    }
 }
