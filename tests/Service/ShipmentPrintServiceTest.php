3b68c35da86be22e24bd964c26f80ceba5ac5bdf
Steponas <steponaskam@gmail.com>
2024-03-12 22:13:13 +0200

    Created ShipmentPrintServiceTest.php file, added testGetShipment() fnc

---
+++ tests/Service/ShipmentPrintServiceTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Invertus\Academy\ShipmentPrintServiceTest;
+
+require_once './src/Service/ShipmentPrintService.php';
+
+use Doctrine\ORM\EntityManagerInterface;
+use Dompdf\Dompdf;
+use Invertus\Academy\Entity\Shipment;
+use Invertus\Academy\ShipmentPrintService\ShipmentPrintService;
+use PHPUnit\Framework\TestCase;
+
+class ShipmentPrintServiceTest extends TestCase
+{
+
+public function testGetShipment()
+{
+    $shipment = new Shipment();
+
+    $mockRepository = $this->createMock(\Doctrine\Persistence\ObjectRepository::class);
+
+    $mockRepository->expects($this->once())
+        ->method('find')
+        ->with(1)
+        ->willReturn($shipment);
+
+    $entityManager = $this->createMock(EntityManagerInterface::class);
+
+    $entityManager->expects($this->once())
+        ->method('getRepository')
+        ->willReturn($mockRepository);
+
+    $shipmentPrintService = new ShipmentPrintService();
+
+    $result = $shipmentPrintService->getShipment($entityManager, 1);
+
+    $this->assertInstanceOf(Shipment::class, $result);
+}
+
+}
