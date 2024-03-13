b59b093f9e38c61e631e88084cedd9ec96bb05bf
Steponas <steponaskam@gmail.com>
2024-03-13 17:48:25 +0200

    Completely remade ShipmentPrintServiceTest.php file

--- tests/Service/ShipmentPrintServiceTest.php
+++ tests/Service/ShipmentPrintServiceTest.php
@@ -1,85 +1,89 @@
 <?php
 
-namespace Tests\Invertus\Academy\ShipmentPrintServiceTest;
+namespace Tests\Invertus\Academy\ShipmentPrintService;
 
-require_once './src/Service/ShipmentPrintService.php';
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Dompdf\Dompdf;
+
 use Invertus\Academy\Entity\Shipment;
 use Invertus\Academy\ShipmentPrintService\ShipmentPrintService;
-use PHPUnit\Framework\TestCase;
 
-class ShipmentPrintServiceTest extends TestCase
+use Dompdf\Dompdf;
+
+class ShipmentPrintServiceTest extends KernelTestCase
 {
+    protected static $container;
+    protected static EntityManagerInterface $entityManager;
+    protected static array $shipmentInformation;
+    public function setUp(): void
+    {
+        self::bootKernel();
+        self::$container = self::getContainer();
+        self::$entityManager = self::$container->get(EntityManagerInterface::class);
+        self::$shipmentInformation = [
+            'country' => 'Lithuania',
+            'company' => 'Test Company',
+            'firstName' => 'Joe',
+            'lastName' => 'Doe',
+            'address1' => '123 Main St.',
+            'address2' => 'Apt 101',
+            'postcode' => '12345',
+            'city' => 'Anytown',
+            'phone' => '123-456-7890',
+            'phoneMobile' => '+3706123123',
+            'barcode' => '10101'
+        ];
+    }
 
     public function testGetShipment()
-    {
+    {   
+        $shipmentPrintService = self::$container->get(ShipmentPrintService::class);
+
         $shipment = new Shipment();
+        $this->addShipmentInformation($shipment);
+        self::$entityManager->persist($shipment);
+        self::$entityManager->flush();
 
-        $mockRepository = $this->createMock(\Doctrine\Persistence\ObjectRepository::class);
+        $returnedShipment = $shipmentPrintService->getShipment(self::$entityManager, $shipment->getId());
 
-        $mockRepository->expects($this->once())
-            ->method('find')
-            ->with(1)
-            ->willReturn($shipment);
-
-        $entityManager = $this->createMock(EntityManagerInterface::class);
-
-        $entityManager->expects($this->once())
-            ->method('getRepository')
-            ->willReturn($mockRepository);
-
-        $shipmentPrintService = new ShipmentPrintService();
-
-        $result = $shipmentPrintService->getShipment($entityManager, 1);
-
-        $this->assertInstanceOf(Shipment::class, $result);
+        self::assertNotNull($returnedShipment);
+        self::assertInstanceOf(Shipment::class, $returnedShipment);
+        self::assertSame($shipment->getId(), $returnedShipment->getId());
     }
 
     public function testGetShipmentInformation()
     {
-        $shipment = new Shipment;
-        $shipment->setCountry('country1');
-        $shipment->setCompany('company2');
-        $shipment->setFirstName('firstName3');
-        $shipment->setLastName('lastName4');
-        $shipment->setAddress1('address15');
-        $shipment->setAddress2('address26');
-        $shipment->setPostcode('postcode7');
-        $shipment->setCity('city8');
-        $shipment->setPhone('phone9');
-        $shipment->setPhoneMobile('phoneMobile10');
-        $shipment->setBarcode(10101);
+        $shipmentPrintService = self::$container->get(ShipmentPrintService::class);
 
-        $shipmentPrintService = new ShipmentPrintService();
-        $shipmentInformation = $shipmentPrintService->getShipmentInformation($shipment);
+        $shipment = new Shipment();
+        $this->addShipmentInformation($shipment);
 
-        $this->assertEquals(
-            [
-                'country'=> 'country1',
-                'company' => 'company2',
-                'firstName' => 'firstName3',
-                'lastName' => 'lastName4',
-                'address1' => 'address15',
-                'address2'=> 'address26',
-                'postcode' => 'postcode7',
-                'city' => 'city8',
-                'phone' => 'phone9',
-                'phoneMobile' => 'phoneMobile10',
-                'barcode' => '10101'
-            ]
-            , $shipmentInformation);
+        $newShipmentInformation = $shipmentPrintService->getShipmentInformation($shipment);
+
+        self::assertSame(self::$shipmentInformation, $newShipmentInformation);
     }
 
     public function testGeneratePdfFile()
     {
-        $shipmentPrintService = new ShipmentPrintService();
+        $shipmentPrintService = self::$container->get(ShipmentPrintService::class);
+        $pdf = $shipmentPrintService->generatePDfFile(self::$shipmentInformation);
 
-        $shipmentInformation = [
-            'country' => 'Great Britain',
-            'barcode' => '10101'
-        ];
-        $this->assertInstanceOf(Dompdf::class, $shipmentPrintService->generatePdfFile($shipmentInformation));
+        self::assertInstanceOf(Dompdf::class, $pdf);
+    }
+
+    private function addShipmentInformation($shipment): void
+    {
+        $shipment->setCountry(self::$shipmentInformation['country']);
+        $shipment->setCompany(self::$shipmentInformation['company']);
+        $shipment->setFirstName(self::$shipmentInformation['firstName']);
+        $shipment->setLastName(self::$shipmentInformation['lastName']);
+        $shipment->setAddress1(self::$shipmentInformation['address1']);
+        $shipment->setAddress2(self::$shipmentInformation['address2']);
+        $shipment->setPostcode(self::$shipmentInformation['postcode']);
+        $shipment->setCity(self::$shipmentInformation['city']);
+        $shipment->setPhone(self::$shipmentInformation['phone']);
+        $shipment->setPhoneMobile(self::$shipmentInformation['phoneMobile']);
+        $shipment->setBarcode(self::$shipmentInformation['barcode']);
     }
 }
